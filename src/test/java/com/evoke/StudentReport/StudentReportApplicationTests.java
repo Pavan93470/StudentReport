82b171ae40ac04d603a0e98064846441a488d2dd
Pavan93470 <rbattila@evoketechnologies.com>
2024-07-11 15:37:00 +0530

    first commit

---
+++ src/test/java/com/evoke/StudentReport/StudentReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoke.StudentReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
